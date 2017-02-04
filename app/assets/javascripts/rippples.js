29c62ad37bfa6d2623ba19e046ae3c10a39fb06e
Andrew Krause <andrew.l.krause@gmail.com>
2017-02-03 20:18:19 -0600

    add styled navbar and footer

---
+++ app/assets/javascripts/rippples.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $(".navbtn").click(function() {
+    $(".navbar").toggleClass("navbar-down");
+  });
+});
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    @page_title = "Main"
+  end
 end
